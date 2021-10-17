951f4eec53044f9582e56e7aaf11e159fc16efc8
Dmitriy Parhomenko <d.parhomenko@belvg.com>
2021-10-17 22:52:52 +0300

    feat: add new task

--- src/components/app-add-form/app-add-form.js
+++ src/components/app-add-form/app-add-form.js
@@ -1,17 +1,43 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import './app-add-form.scss';
 
-const AppAddForm = ({btnName, onAddItem}) => {
-    return (
-        <div className="app-add-form">
-            <button type="button"
-                    className="btn btn-outline-secondary"
-                    onClick={() => onAddItem('Hello')}>
-                {btnName}
-            </button>
-        </div>
-    )
-}
+export default class AppAddForm extends Component {
 
-export default AppAddForm;
+    state = {
+        label: ''
+    };
+
+    onLabelChange = (e) => {
+        this.setState({
+            label: e.target.value
+        });
+    };
+
+    onSubmit = (e) => {
+        e.preventDefault();
+
+        this.props.onAddItem(this.state.label);
+        this.setState({
+            label: ''
+        });
+    };
+
+    render() {
+        const {btnName} = this.props;
+
+        return (
+            <form className="app-add-form"
+                  onSubmit={this.onSubmit} >
+                <input type="text"
+                       value={this.state.label}
+                       className="form-control"
+                       onChange={this.onLabelChange}
+                       placeholder="Your task" />
+                <button className="btn btn-outline-secondary" >
+                    {btnName}
+                </button>
+            </form>
+        );
+    }
+}
